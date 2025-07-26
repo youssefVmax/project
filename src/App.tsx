703d7a8c1c7e47f5b6718940ad550454dd270184
youssefVmax <youssefbassiony@vmaxcom.com>
2025-07-26 23:14:20 +0200

    manus fix fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useTheme } from './hooks/useTheme';
 import { Navigation } from './components/Navigation';
-import  Dashboard  from './pages/Dashboarded7fix.tsx'
+import  Dashboard  from './pages/Dashboarded7fix (1).tsx'
 import { DataEntry } from './pages/DataEntry';
 import { SalesTable } from './pages/SalesTable_improved.tsx';
 import CustomerPrediction from './pages/enhanced_prediction_component.tsx';
